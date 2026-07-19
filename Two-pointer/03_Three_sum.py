f0bdf001411a075455ce02dce20147b170535d7a
Shristi <164264996+Cybersphinix@users.noreply.github.com>
2026-07-19 10:34:57 -0500

    Added a new file

--- Two-pointer/03_Three_sum.py
+++ Two-pointer/03_Three_sum.py
@@ -34,18 +34,102 @@
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         
+        # Because the output should not contain the duplicate triplets:
         output=set()
         for i in range(0,len(nums)):
-            for j in range(0,len(nums)):
-                for k in range(0,len(nums)):
+            for j in range(i+1,len(nums)):
+                for k in range(j+1,len(nums)):
                     if i!=j and j!=k and i!=k and nums[i]+nums[j]+nums[k]==0:
                             triplet = tuple(sorted([nums[i], nums[j], nums[k]]))
                             output.add(triplet)
 
         return [list(t) for t in output]
         
-# Time complexity = O(n^3)
+# Time complexity = O(n^3) 
 # Space complexity = O(m) -> m is no of unique triplets
+
+
+# Better solution:
+
+ # array[k] = - (array[i] + array[j])
+
+ # [-1,0,1,2,-1,-4] i is fixed , create a set:
+
+ 
+   output= set()
+   n = len(nums)
+
+    for i in range(0,n):
+        hashset=set()
+        for j in range(i+1,n):
+            third = -(nums[i]+nums[j])
+            if third in hashset:
+                triplet = tuple(sorted([nums[i],nums[j],third]))
+                output.add(triplet)
+            hashset.add(nums[j])
+    return [list(t) for t in output]
+
+
+# Time complexity = O(n^2)
+# Space complexity = O(n+m) { m= no of unique elements}
+
+
+
+# The optimized solution:
+
+ [-4, -1, -1, 0, 1, 2]
+
+
+  ans=[]
+  nums.sort()
+  n=len(nums)
+
+  for i in range(n):
+        if i > 0 and nums[i]==nums[i-1]:
+            continue
+        j=i+1
+        k=n-1
+
+        while j<k:
+            total=nums[i]+nums[j]+nums[k]
+
+            if total>0:
+                k-=1
+            elif total<0:
+                j+=1
+            else:
+                triplet=[nums[i],nums[j],nums[k]]
+                ans.append(triplet)
+                j+=1
+                k-=1
+
+                while j<k and nums[j]==nums[j-1]:
+                    j+=1
+                while j<k and nums[k]==nums[k+1]:
+                    k-=1
+        return ans 
+
+# (No use of extra data structures)
+# Time complexity =  O(nlogn) + O(n^2) 
+# Space complexity = O(m) where m is no of unique elements
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 # Optimized solution: Using two pointer solution:
